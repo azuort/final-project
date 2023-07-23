f70c83f89c1b617a2f888f7b6fe3e5ccbda41a6b
azuort01~ <azucenaperdomo00@gmil.com>
2023-07-22 21:18:41 -0600

    "Instalacion Route"

---
+++ views/frontend/src/Components/Blog/AllBlogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AllBlogs = () => {
+  return (
+    <div>
+      <h2>All Blogs</h2>
+      {/* Contenido específico de la página "Acerca de" */}
+    </div>
+  );
+};
+
+export default AllBlogs;
---
+++ views/frontend/src/Components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to the Home Page!</h2>
+      {/* Contenido específico de la página de inicio */}
+    </div>
+  );
+};
+
+export default Home;
